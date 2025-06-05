0544beab30f903b498f24e79cf5584b18c4788c7
dunIsBruh <daniargarafiev6@gmail.com>
2025-06-05 11:11:08 +0300

    Add cors middleware

--- server/main.py
+++ server/main.py
@@ -8,7 +8,7 @@ from api_v1 import router as api_v1_router
 
 import uvicorn
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -19,6 +19,13 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.include_router(api_v1_router, prefix=settings.api_v1_prefix)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5500"],  # Только ваш фронтенд
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["Content-Type", "Authorization"],
+)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
